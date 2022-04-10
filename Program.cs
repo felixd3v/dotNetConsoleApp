a767ab80f8a6092a98bce229c1c13cb419ecb3d6
felixd3v <fmhcde@gmail.com>
2022-04-09 20:18:27 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, C# user");
+
+string userResponse;
+Console.WriteLine("User, what is your name?");
+userResponse = Console.ReadLine();
+Console.WriteLine($"Enjoy your learning journey, {userResponse}");
